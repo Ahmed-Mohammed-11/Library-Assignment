e86051a3f09c33bf1704401e86ac1438f87be802
AXm0us <74784731+Ahmed-Mohammed-11@users.noreply.github.com>
2024-04-15 22:25:54 +0200

    Update BookIsBorrowedExceptionHandler.java

--- src/main/java/com/books/library/exceptions/handlers/BookIsBorrowedExceptionHandler.java
+++ src/main/java/com/books/library/exceptions/handlers/BookIsBorrowedExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@RestControllerAdvice
 public class BookIsBorrowedExceptionHandler {
     @ExceptionHandler(BookIsBorrowedException.class)
     public ResponseEntity<String> handleBookIsBorrowed(BookIsBorrowedException e) {
