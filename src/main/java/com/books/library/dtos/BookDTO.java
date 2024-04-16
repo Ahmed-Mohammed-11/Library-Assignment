b3225a7c3cc978bd9f7a01d302b6affbf7844ae7
Ahmed-Mohammed-11 <ahmedmo1162002@gmail.com>
2024-04-16 09:38:26 +0200

    minor bug fix

--- src/main/java/com/books/library/dtos/BookDTO.java
+++ src/main/java/com/books/library/dtos/BookDTO.java
@@ -1,13 +1,13 @@
 package com.books.library.dtos;
 
 import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.springframework.format.annotation.NumberFormat;
 
 import static com.books.library.constants.Regex.*;
 import static com.books.library.constants.ResponseMessageConstants.*;
@@ -18,14 +18,17 @@ import static com.books.library.constants.ResponseMessageConstants.*;
 @SuperBuilder
 public class BookDTO {
 
+    @NotNull
     @Size(min = 1, max = 255, message = BOOK_TITLE_SIZE)
     @Pattern(regexp = TITLE_REGEX, message = BOOK_TITLE_FORMAT)
     private String title;
 
+    @NotNull
     @Size(min = 1, max = 255, message = BOOK_AUTHOR_SIZE)
     @Pattern(regexp = AUTHOR_REGEX, message = BOOK_AUTHOR_FORMAT)
     private String author;
 
+    @NotNull
     @Digits(integer = 4, fraction = 0, message = BOOK_PUBLICATION_YEAR_FORMAT)
     private int publicationYear;
 
