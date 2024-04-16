553c3e0a5022444aa59f7d44facd203b0c31d73a
Ahmed-Mohammed-11 <ahmedmo1162002@gmail.com>
2024-04-16 09:03:24 +0200

    minor bug fix

--- src/main/java/com/books/library/exceptions/handlers/BookIsBorrowedExceptionHandler.java
+++ src/main/java/com/books/library/exceptions/handlers/BookIsBorrowedExceptionHandler.java
@@ -4,6 +4,7 @@ import com.books.library.exceptions.exceptions.BookIsBorrowedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class BookIsBorrowedExceptionHandler {
--- src/main/java/com/books/library/exceptions/handlers/BookIsNotBorrowedExceptionHandler.java
+++ src/main/java/com/books/library/exceptions/handlers/BookIsNotBorrowedExceptionHandler.java
@@ -4,6 +4,7 @@ import com.books.library.exceptions.exceptions.BookIsNotBorrowedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class BookIsNotBorrowedExceptionHandler {
